8a633f725e3d91e8bfbb424397a679ac12f8899a
Vincent <vangell@videotron.ca>
2019-01-19 14:08:00 -0500

    Vue principale ajouté par maxime

---
+++ client/src/app/principal-view/principal-view/principal-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-principal-view',
+  templateUrl: './principal-view.component.html',
+  styleUrls: ['./principal-view.component.css']
+})
+export class PrincipalViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
